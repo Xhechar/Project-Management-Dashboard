c929e54519a90cd3f534c2436686faa5927e47cb
Xhechar <115901405+Xhechar@users.noreply.github.com>
2024-05-18 01:45:27 +0300

    assessment complete task

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ let dynamicBody = document.querySelector('.dynamic-body');
 
 let projectArray = [];
 
+let viewOneClicked = false;
+let viewAllClicked = false;
+
 addBtn.addEventListener('click', () => {
 
   console.log('clicked');
@@ -54,37 +57,122 @@ function displayProjects() {
   }
   console.log('in a function');
 
-  retrievedProjects.forEach((project, index) => {
+  if (viewAllClicked) {
+    retrievedProjects.forEach((project, index) => {
 
-    let displayedDiv = document.createElement('div');
-    displayedDiv.className = 'each-project'
-    
-    let displayedProjectItem = document.createElement('p');
-    displayedProjectItem.textContent = project.newItem;
-
-    let deleteBtn = document.createElement('button');
-    deleteBtn.className = 'delete-project';
-    deleteBtn.textContent = "Delete";
-
-    displayedDiv.appendChild(displayedProjectItem);
-    displayedDiv.appendChild(deleteBtn);
-
-    dynamicBody.appendChild(displayedDiv);
-
-    console.log('deepin another function');
-
-    deleteBtn.addEventListener('click', () => {
-      deleteItem = index;
-      console.log(deleteBtn);
-      if (deleteItem === '') {
-        console.log('empty');
-      }
-      else {
-        projectArray.splice(deleteItem, 1);
-        localStorage.setItem('storage', JSON.stringify(projectArray));
-      }
-      displayProjects();
+      let displayedDiv = document.createElement('div');
+      displayedDiv.className = 'each-project'
+      
+      let displayedProjectItem = document.createElement('p');
+      displayedProjectItem.textContent = project.newItem;
+  
+      let deleteBtn = document.createElement('button');
+      deleteBtn.className = 'delete-project';
+      deleteBtn.textContent = "Delete";
+  
+      displayedDiv.appendChild(displayedProjectItem);
+      displayedDiv.appendChild(deleteBtn);
+  
+      dynamicBody.appendChild(displayedDiv);
+  
+      console.log('deepin another function');
+  
+      deleteBtn.addEventListener('click', () => {
+        deleteItem = index;
+        console.log(deleteBtn);
+        if (deleteItem === '') {
+          console.log('empty');
+        }
+        else {
+          projectArray.splice(deleteItem, 1);
+          localStorage.setItem('storage', JSON.stringify(projectArray));
+        }
+        displayProjects();
+      })
     })
-  })
+  }
+  else if (viewOneClicked) {
 
+    retrievedProjects.forEach((project, index) => {
+
+      if (project.newItem == inputBar.value) {
+        let displayedDiv = document.createElement('div');
+        displayedDiv.className = 'each-project'
+        
+        let displayedProjectItem = document.createElement('p');
+        displayedProjectItem.textContent = project.newItem;
+    
+        let deleteBtn = document.createElement('button');
+        deleteBtn.className = 'delete-project';
+        deleteBtn.textContent = "Delete";
+    
+        displayedDiv.appendChild(displayedProjectItem);
+        displayedDiv.appendChild(deleteBtn);
+    
+        dynamicBody.appendChild(displayedDiv);
+    
+        console.log('deepin another function');
+    
+        deleteBtn.addEventListener('click', () => {
+          deleteItem = index;
+          console.log(deleteBtn);
+          if (deleteItem === '') {
+            console.log('empty');
+          }
+          else {
+            projectArray.splice(deleteItem, 1);
+            localStorage.setItem('storage', JSON.stringify(projectArray));
+          }
+          displayProjects();
+        })
+      }
+    })
+  }
+  else {
+    retrievedProjects.forEach((project, index) => {
+
+      let displayedDiv = document.createElement('div');
+      displayedDiv.className = 'each-project'
+      
+      let displayedProjectItem = document.createElement('p');
+      displayedProjectItem.textContent = project.newItem;
+  
+      let deleteBtn = document.createElement('button');
+      deleteBtn.className = 'delete-project';
+      deleteBtn.textContent = "Delete";
+  
+      displayedDiv.appendChild(displayedProjectItem);
+      displayedDiv.appendChild(deleteBtn);
+  
+      dynamicBody.appendChild(displayedDiv);
+  
+      console.log('deepin another function');
+  
+      deleteBtn.addEventListener('click', () => {
+        deleteItem = index;
+        console.log(deleteBtn);
+        if (deleteItem === '') {
+          console.log('empty');
+        }
+        else {
+          projectArray.splice(deleteItem, 1);
+          localStorage.setItem('storage', JSON.stringify(projectArray));
+        }
+        displayProjects();
+      })
+    })
+
+  }
 }
+
+viewOne.addEventListener('click', () => {
+  viewOneClicked = true;
+  displayProjects();
+  viewOneClicked = false;
+})
+
+viewAll.addEventListener('click', () => {
+  viewAllClicked = true;
+  displayProjects();
+  viewAllClicked = false;
+})
